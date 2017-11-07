81980b026a1166b337356f5cd293d7d877fb09a0
Ian Phillips <ianp@ianp.org>
2017-11-07 09:43:27 +0000

    Added usage note to TS declarations

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,15 @@
-// Type definitions for @naytev/grapheme-splitter
+// Type definitions for grapheme-splitter
+
+/*~ Note that ES6 modules cannot directly export class objects.
+ *~ This file should be imported using the CommonJS-style:
+ *~
+ *~   import GraphemeSplitter = require('grapheme-splitter')
+ *~
+ *~ Refer to the documentation to understand common
+ *~ workarounds for this limitation of ES6 modules.
+ *~
+ *~   https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
+ */
 
 declare class GraphemeSplitter {
   constructor()
